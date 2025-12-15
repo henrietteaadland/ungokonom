0b46bacaf9a4c4af701193f5b88d751cfed3989f
SlimSham <shdu002@student.kristiania.no>
2025-12-15 13:03:11 +0100

    Legger til Infoside og Quiz

---
+++ Smidig-Prosjekt/Infoside/accordion.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".bare-header").forEach((button) => {
+    button.addEventListener("click", () => {
+        const item = button.parentElement;
+        item.classList.toggle("open")
+    });
+});
