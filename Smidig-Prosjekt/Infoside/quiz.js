1739587540b1780639689165332158290a4677f7
SlimSham <shdu002@student.kristiania.no>
2025-12-16 10:42:34 +0100

    Ferdig

--- Smidig-Prosjekt/Infoside/quiz.js
+++ Smidig-Prosjekt/Infoside/quiz.js
@@ -2,100 +2,100 @@ const questions = [
     {
         question: "Hva er akseptpris?",
         options: [
-            "s",
-            "s",
-            "s",
+            "Den laveste prisen en kjøper vil godta",
+            "Løsning som sikrer at dine penger er sikre",
+            "Anbefalte kjøpspris av megler",
         ],
         correctIndex: 0
     },
 
     {
-        question: "hva kjennetegner et annuitetslån?",
+        question: "Hva kjennetegner et annuitetslån?",
         options: [
-            "s",
-            "s",
-            "s",
+            "At du betaler mindre hver måned",
+            "Betaling av samme beløp hver måned",
+            "En satt periode du har på deg for å betale tilbake",
         ],
         correctIndex: 1
     },
     {
-        question: "hva er egenkapital?",
+        question: "Hva er egenkapital?",
         options: [
-            " ",
-            " ",
-            " ",
-        ],
-        correctIndex: 1
-    },
-
-    {
-        question: "hva viser et forbrukslån?",
-        options: [
-            " ",
-            " ",
-            " ",
+            "Penger du låner",
+            "Penger du har",
+            "Totale summen av alle lånene dine",
         ],
         correctIndex: 1
     },
 
     {
-        question: "hva betyr fastrente?",
+        question: "Hva viser et forbrukslån?",
         options: [
-            " ",
-            " ",
-            " ",
+            "Et lån du kan få for å kjøpe bolig",
+            "Et lån av penger uten sikkerhet",
+            "Kostnader itillegg til lånet du tar",
         ],
         correctIndex: 1
+    },
+
+    {
+        question: "Hva er et serielån?",
+        options: [
+            "Penger du låner av andre, ved å skrive under et dokument",
+            "Et lån der du betaler samme beløp hver måned",
+            "Et lån der du betaler mindre hver måned",
+        ],
+        correctIndex: 2
     },
 
     {
         question: "hva er gjeld?",
         options: [
-            " ",
-            " ",
-            " ",
+            "Penger du skylder",
+            "Penger i din konto",
+            "Penger du låner bort",
         ],
-        correctIndex: 1
+        correctIndex: 0
+    },
+
+    {
+        question: "Hva er nominell rente",
+        options: [
+            "Renten bare øker, synker ikke",
+            "Renter uten gebyrer",
+            "Renter du må betale på lånet ditt",
+        ],
+        correctIndex: 2
     },
 
     {
         question: "hva er en nedbetaligsplan?",
         options: [
-            " ",
-            " ",
-            " ",
+            "Totale summen av hva du skylder",
+            "En oversikt over dine regninger",
+            "plan som viser hvor mye du må betale og hvor lang tid det vil ta",
         ],
-        correctIndex: 1
+        correctIndex: 2
     },
 
     {
-        question: "hva er en nedbetaligsplan?",
+        question: "Hva vil det si at du har fått en faktura?",
         options: [
-            " ",
-            " ",
-            " ",
+            "Dokument som viser hva du må betale",
+            "Kostnader for å drive en bedrift",
+            "Et brev du for når du ikke har betalt det du skylder",
         ],
-        correctIndex: 1
+        correctIndex: 0
     },
 
     {
-        question: "hva er en nedbetaligsplan?",
+        question: "Hva er betydninger av kausjon?",
         options: [
-            " ",
-            " ",
-            " ",
+            "At noen lover å betale for deg, dersom du ikke kan betale",
+            "Oversikt over banken du er knyttet til",
+            "Liste over dine regninger",
         ],
-        correctIndex: 1
-    },
-
-    {
-        question: "hva er en nedbetaligsplan?",
-        options: [
-            " ",
-            " ",
-            " ",
-        ],
-        correctIndex: 1
+        correctIndex: 0
     },
 ];
 
@@ -124,7 +124,7 @@ questions.forEach((q, qIndex) => {
     q.options.forEach((optionText, optionIndex) => {
         const button = document.createElement("button");
         button.className = "quiz-option";
-        button.textContent = optionIndex;
+        button.textContent = optionText; /* DENNE SLØSTE MASSE TID, HADDE OPTIONINDEX, SOM DA VISTE TALL OG IKKE SPØRSMÅL */
         button.dataset.questionIndex = qIndex.toString();
         button.dataset.optionIndex = optionIndex.toString();
 
