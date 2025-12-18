2b704050476681eeed8946c9f1e2d015b363e47c
SlimSham <150444830+SlimSham@users.noreply.github.com>
2025-12-18 21:56:32 +0100

    ReadMe.f
    Hvordan løsningen kan lages, basert på kildekoden med beskrivelser av krav til arbeidsmiljø

---
+++ ReadMe.f
@@ -0,0 +1,28 @@
+Ung økonom
+Dette er en enkel webapplikasjon laget i HTML, CSS og Javascript. Løsningen består av tre sider:
+
+- Lånekalkulator: der bruker kan fylle inn sine verdier og utifra dem få visualiseringer
+- Begreper: En oversikt over økonomiske begreper
+- Quiz: en enkel quiz der bruker kan teste sin kunnskap fra begreper siden
+
+Teknologier som er brukt
+- HTML: struktur for applikasjonen
+- CSS: styling, layout, farger osv
+- Javascript: for interaktivitet
+            - vise grafer og kalkulering 
+            - åpne/lukke forklaringer på begreper
+            - generering av quiz spørsmål, svar alternativer og poengregning
+
+
+Krav til utviklingsmiljø
+For å kjøre og videreutvikle løsningen trengs følgende:
+
+- en nettleser (f.eks chrome, google, firefox osv)
+- en kodeeditor, for eksempel:
+  - [visual studio code] (anbefalt)
+  - **live server** for å kjøre koden lokalt med god refresh
+
+
+Hvordan kjøre prosjektet lokalt
+
+  
